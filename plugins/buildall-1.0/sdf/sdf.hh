6c6990f2ce387367c404fcb771a1b24f5d589803
Tiberio <tiberiusferreira@gmail.com>
2013-10-04 04:17:33 -0300

    Included proper make files so you can use it to compile in any pc, hopefully. You can also use "make install" command to quickly install the plugins. As it copies to system folder you need to execute as sudo. The make process already puts one of the files in a system folder so it's properly linked, so it also needs the sudo permission. Added a install script file so ros and gazebo can be easily installed and setup just executing the script (as of now). Tried to update the car values too.

---
+++ plugins/buildall-1.0/sdf/sdf.hh
@@ -0,0 +1,11 @@
+// Automatically generated
+ #include <sdf/Assert.hh>
+ #include <sdf/Console.hh>
+ #include <sdf/Converter.hh>
+ #include <sdf/Exception.hh>
+ #include <sdf/Param.hh>
+ #include <sdf/parser.hh>
+ #include <sdf/SDFImpl.hh>
+ #include <sdf/Types.hh>
+
+#include <sdf/sdf_config.h>
