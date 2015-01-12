79c583e2262ed9c042ece704ddece361da73a6dc
Tiberio Ferreira <tiberiusferreira@gmail.com>
2015-01-12 08:35:56 -0200

    Automatic Driving to Point Working =) !

--- src/vilma_driver_1_0/src/vilma_self_driver.cpp
+++ src/vilma_driver_1_0/src/vilma_self_driver.cpp
@@ -11,33 +11,32 @@ int vilma_self_driver::reorientate_to_pose(float x, float y){ //reorientates whe
     //Solution: put all data in respect to car. Take car as origin, but keep axis orientation.
     float newx=x-morse_receiver_obj->getPosX();
     float newy=y-morse_receiver_obj->getPosY();
-    if( (newy>=0 && (morse_receiver_obj->getOrientationZAsEuler()<=(-3.14/2) || morse_receiver_obj->getOrientationZAsEuler()>=(3.14/2)))
-            || (newy<0 && ((morse_receiver_obj->getOrientationZAsEuler()<(3.14/2)) && morse_receiver_obj->getOrientationZAsEuler()>(-3.14/2))))
-    { //if point is oposite to car direction, do nothing
-        /* The point given is oposite to car direction if the car is facing forward (between -pi/2 and pi/2 of rotation)
- and the point has a negative x value or if the car is facing backwards (between -pi/2 and -pi or between pi/2 and pi) and
-the point has a positive x value.*/
-        return -1;
-    }
+//    if( (newy>=0 && (morse_receiver_obj->getOrientationZAsEuler()<=(-3.14/2) || morse_receiver_obj->getOrientationZAsEuler()>=(3.14/2)))
+//            || (newy<0 && ((morse_receiver_obj->getOrientationZAsEuler()<(3.14/2)) && morse_receiver_obj->getOrientationZAsEuler()>(-3.14/2))))
+//    { //if point is oposite to car direction, do nothing
+//        /* The point given is oposite to car direction if the car is facing forward (between -pi/2 and pi/2 of rotation)
+// and the point has a negative x value or if the car is facing backwards (between -pi/2 and -pi or between pi/2 and pi) and
+//the point has a positive x value.*/
+//        return -1;
+//    }
     float ang=atan(newx/newy); //ang returned is from -pi/2 to pi/2
     float ang_car=morse_receiver_obj->getOrientationZAsEuler(); //get car z rotation, but it goes from 0 to -3.14 and 0 to 3.14 too
     //need to convert it so I can compare it with ang_car. It only needs to happen when the car is backwards
-    if(ang_car>(3.14/2))
-    {
-        ang_car=(ang_car-(3.14/2));
-    }else if(ang_car<(-3.14/2))
-    {
-        ang_car=((3.14/2)+(ang_car));
+    if(newx>0 && newy>0){ //first quadrant
+        ang=-ang;
+    }else if(newx<0 && newy>0){ //second quadrant
+        ang=-ang;
+    }else if(newx<0 && newy<0){ //third quadrant
+        ang=3.14-ang;
+    }else if(newx>0 && newy<0){ //forth quadrant
+        ang=-3.14-ang;
     }
-    ang_car=-ang_car;
     float value_to_turn=ang-ang_car;
     qDebug("Car Z is: %f\n",morse_receiver_obj->getOrientationZAsEuler());
     qDebug("Delta X is: %f\n",newx);
     qDebug("Detla Y is: %f\n",newy);
-
-    qDebug("The world ang to reorient is:%f\n",ang);
-    qDebug("The car ang is:%f\n",ang_car);
-    morse_transmiter_obj->setSteering(-value_to_turn);
+    qDebug("Atan newx/newy is:%f\n",ang);
+    morse_transmiter_obj->setSteering(value_to_turn);
     return 1;
 
 }
